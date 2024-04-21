e7e34d91d3c2410528ff0af4ea13b11b8297eff3
JolyMolyf <vs2001dor@gmail.com>
2024-04-21 19:26:48 +0200

    Added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights/>
         <ReduxProvider>
           <NavBar/>
           {children}
